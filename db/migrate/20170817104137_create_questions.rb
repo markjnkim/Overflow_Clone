7d60536599c90babe257d31b2f24aa73169aa174
Alexis DeRuyter <email": null>
2017-08-17 11:08:06 -0700

    Add migration and model

--- db/migrate/20170817104137_create_questions.rb
+++ db/migrate/20170817104137_create_questions.rb
@@ -1,4 +1,11 @@
 class CreateQuestions < ActiveRecord::Migration[5.1]
   def change
+    create_table :questions do |t|
+      t.string :title, null: false
+      t.string :body
+      t.integer :user_id, null: false
+
+      t.timestamps
+    end
   end
 end
