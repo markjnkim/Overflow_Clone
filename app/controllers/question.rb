f08d0ad7d6f8f5b74c8bba321cc257480e265ee5
Alexis DeRuyter <email": null>
2017-08-17 11:14:28 -0700

    Add question controller with restful routes template

---
+++ app/controllers/question.rb
@@ -0,0 +1,34 @@
+#index
+get '/questions' do
+
+end
+
+#show
+get '/questions/:id' do
+
+end
+
+#new
+get '/questions/new' do
+
+end
+
+#create
+post '/questions' do
+
+end
+
+#edit
+get '/questions/:id/edit' do
+
+end
+
+#update
+patch '/questions/:id' do
+
+end
+
+#delete
+delete '/questions/:id' do
+
+end
