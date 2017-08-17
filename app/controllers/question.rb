e1da270be618e919adbde4396405045bb557d3f5
Alexis DeRuyter <email": null>
2017-08-17 12:21:32 -0700

    Add views

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -1,34 +1,46 @@
 #index
 get '/questions' do
-
-end
-
-#show
-get '/questions/:id' do
-
+  @questions = Question.all
+  slim :'question/index'
 end
 
 #new
 get '/questions/new' do
+  @question = Question.new
+  slim :'question/_new'
+end
 
+#show
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+  slim :'question/show'
 end
 
 #create
 post '/questions' do
-
+  # FOR TESTING
+  params[:question][:user_id] = 3
+  # USE WHEN USERS IS IMPLEMENTED
+  # question[:user] = current_user
+  @question = Question.create(params[:question])
+  if @question.errors.any?
+    slim :'question/_new'
+  else
+    redirect "/questions/#{question.id}"
+  end
 end
 
 #edit
-get '/questions/:id/edit' do
+# get '/questions/:id/edit' do
 
-end
+# end
 
 #update
-patch '/questions/:id' do
+# patch '/questions/:id' do
 
-end
+# end
 
 #delete
-delete '/questions/:id' do
+# delete '/questions/:id' do
 
-end
+# end
